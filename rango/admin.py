f1979418c64d3646f366a8e5009c40958c0ba6c3
Akib Aman <2659552a@student.gla.ac.uk>
2023-01-26 18:39:56 +0000

    week 5 and 6

--- rango/admin.py
+++ rango/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-
+from rango.models import Category, Page
 # Register your models here.
 
-from django.contrib import admin
-from rango.models import Category, Page
-admin.site.register(Category)
-admin.site.register(Page)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    
+admin.site.register(Category, CategoryAdmin)
